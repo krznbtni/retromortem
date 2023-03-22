c3b75a64da34128a436d1b88b835672b4937e733
Karzan Botani <karzan@botani.nu>
2023-03-22 21:54:53 +0100

    refactor(login): allow multiple oauth providers

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,28 +1,36 @@
+import { OAUTH_REDIRECT_URL } from '$lib/constants';
+import type { OAuthLink } from '$lib/types/oauth';
 import { redirect } from '@sveltejs/kit';
-import type {  PageServerLoad } from './$types';
-// import type { ClientResponseError } from 'pocketbase';
+import type {  PageServerLoad, RequestEvent } from './$types';
 
-export const load = (async ({ locals, url }) => {
+export const load = (async (event) => {
+  const {locals} = event;
+
+  // Redirect to startpage if already authenticated.
 	if (locals.pb.authStore.isValid) {
 		throw redirect(303, '/');
 	}
 
-  const authMethods = await locals.pb.collection('users').listAuthMethods();
-
-  if (!authMethods) {
-    return {
-      authProviderRedirect: '',
-      authProviderState: ''
-    }
-  }
-
-  const redirectURL = `${url.origin}/api/oauth2`;
-  const githubAuthProvider = authMethods.authProviders[0];
-  const authProviderRedirect = `${githubAuthProvider.authUrl}${redirectURL}`
-  const authProviderState = githubAuthProvider.state;
-
   return {
-    authProviderRedirect,
-    authProviderState
+    oauthLinks: buildOauthLinks(event)
   }
 }) satisfies PageServerLoad;
+
+async function buildOauthLinks({locals, url}: RequestEvent): Promise<Array<OAuthLink>> {
+  const redirectURL = `${url.origin}${OAUTH_REDIRECT_URL}`;
+  const oauthLinks: Array<OAuthLink> = [];
+
+  const authMethods = await locals.pb.collection('users').listAuthMethods();
+
+  for (const provider of authMethods.authProviders) {
+    const oauthLink: OAuthLink = {
+      name: provider.name,
+      href: provider.authUrl + redirectURL,
+      state: provider.state
+    }
+
+    oauthLinks.push(oauthLink);
+  }
+
+  return oauthLinks;
+}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
+	import type { OAuthLink } from '$lib/types/oauth';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	function gotoAuthProvider() {
+	$: oauthLinks = data.oauthLinks;
+	$: noLinks = oauthLinks.length === 0;
+
+	function gotoAuthProvider(oauthLink: OAuthLink) {
 		if (browser) {
-			document.cookie = `state=${data?.authProviderState}`;
+			document.cookie = `state=${oauthLink.state};`;
+			document.cookie = `name=${oauthLink.name};`;
 		}
 
-		window.location.href = data.authProviderRedirect || '';
+		window.location.href = oauthLink.href;
 	}
 </script>
 
-<button on:click={gotoAuthProvider}>Login with GitHub babes</button>
+{#if noLinks}
+	<h2>Awwwwwwwww, no OAuthLinks</h2>
+{:else}
+	{#each oauthLinks as oauthLink (oauthLink.name)}
+		<button on:click={() => gotoAuthProvider(oauthLink)}>{`Login with ${oauthLink.name}`}</button>
+	{/each}
+{/if}
