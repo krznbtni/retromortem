b6099ba330b80a1ba8391438adc1fbc18900961b
Botani <22057746+krznbtni@users.noreply.github.com>
2023-03-23 06:20:12 +0100

    chore: add 'satisfactory' ESLint configuration (#12)
    * chore: add linting deps
    
    * refactor: add basic eslint config
    
    * chore(deps): bump all packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,55 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
-	}
+		node: true,
+	},
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+		extraFileExtensions: ['.svelte'],
+		sourceType: 'module',
+		ecmaVersion: 2020,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:svelte/recommended',
+		'plugin:svelte/prettier',
+		'plugin:prettier/recommended',
+	],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				sourceType: 'module',
+				ecmaVersion: 2020,
+				parser: '@typescript-eslint/parser',
+			},
+		},
+		{
+			files: ['src/**/*.test.ts'],
+			extends: ['plugin:vitest/recommended'],
+		}
+	],
+	rules: {
+		// JavaScript
+		'arrow-body-style': ['error'],
+		'no-unneeded-ternary': ['error', {defaultAssignment: false}],
+		'no-unused-expressions': ['error'],
+		'prefer-arrow-callback': ['error'],
+
+		// TypeScript
+		'@typescript-eslint/consistent-type-imports': ['error'],
+		'@typescript-eslint/no-inferrable-types': ['error'],
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{argsIgnorePattern: '^_', varsIgnorePattern: '^_'},
+		],
+	},
+	ignorePatterns: ['*.cjs', 'svelte.config.js', 'node_modules'],
 };
