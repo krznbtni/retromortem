9d97f9330e69819d1a7909a0c5ee2ec3e44534dd
Botani <22057746+krznbtni@users.noreply.github.com>
2023-03-24 20:08:31 +0100

    feat: pretty login page (#32)
    * build: prettify postcss config
    
    * refactor: prettify login page
    
    * eat: add loading indicator when logging in

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,10 +5,17 @@ import type {PageData} from './$types';
 
 export let data: PageData;
 
+let loading = false;
+
 $: oauthLinks = data.oauthLinks;
-$: noLinks = oauthLinks.length === 0;
 
 function gotoAuthProvider(oauthLink: OAuthLink) {
+  if (loading) {
+    return;
+  }
+
+  loading = true;
+
   if (browser) {
     document.cookie = `state=${oauthLink.state};`;
     document.cookie = `name=${oauthLink.name};`;
@@ -18,12 +25,16 @@ function gotoAuthProvider(oauthLink: OAuthLink) {
 }
 </script>
 
-{#if noLinks}
-  <h2>Awwwwwwwww, no OAuthLinks</h2>
-{:else}
-  {#each oauthLinks as oauthLink (oauthLink.name)}
-    <button on:click={() => gotoAuthProvider(oauthLink)}>{`Login with ${oauthLink.name}`}</button>
-  {/each}
-{/if}
+<div class="card w-96 bg-base-100 shadow-xl mx-auto my-16">
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">Hey cutie! Log in below</h2>
 
-<style lang="postcss"></style>
+    <div class="card-actions">
+      {#each oauthLinks as oauthLink (oauthLink.name)}
+        <button class="btn" class:loading on:click={() => gotoAuthProvider(oauthLink)}>
+          Log in with {oauthLink.name}
+        </button>
+      {/each}
+    </div>
+  </div>
+</div>
