535777f35df24065322182cc334875e4fe434e22
Karzan Botani <karzan@botani.nu>
2023-03-24 23:51:36 +0100

    refactor: update component barrel file

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,1 +1,5 @@
+export {default as DatePicker} from '$lib/components/common/DatePicker.svelte';
+export {default as Input} from '$lib/components/common/Input.svelte';
+export {default as Select} from '$lib/components/common/Select.svelte';
+export {default as TextArea} from '$lib/components/common/TextArea.svelte';
 export {default as NavBar} from '$lib/components/layout/NavBar.svelte';
