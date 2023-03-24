d241783844e1c4553f816ba4e7d24eaefcf02675
Karzan Botani <karzan@botani.nu>
2023-03-24 19:45:56 +0100

    build: prettify postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,15 +1,7 @@
-const plugins =
-  process.env.NODE_ENV !== 'development'
-    ? {
-        tailwindcss: {},
-        'postcss-preset-env': {stage: 1},
-        cssnano: {},
-      }
-    : {
-        tailwindcss: {},
-        'postcss-preset-env': {stage: 1},
-      };
-
 module.exports = {
-  plugins,
+  plugins: {
+    tailwindcss: {},
+    'postcss-preset-env': {stage: 1},
+    ...(process.env.NODE_ENV !== 'development' ? {cssnano: {}} : {}),
+  },
 };
