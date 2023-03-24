87e571cd7aeeccf8a29ce1bebcc94b64d9a7b1bf
Karzan Botani <karzan@botani.nu>
2023-03-24 23:46:43 +0100

    feat: add Select component

---
+++ src/lib/components/common/Select.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+export let value: unknown;
+export let label = '';
+export let disabled = false;
+export let required = false;
+export let options: Array<unknown> = [];
+export let id = '';
+
+$: {
+  value = options[0];
+}
+</script>
+
+<div class="form-control w-full max-w-lg mb-2">
+  <label class="label" for={id}>
+    <span class="label-text">{label}</span>
+  </label>
+
+  <select class="select select-bordered" {required} {disabled} {value} {id} name={id}>
+    {#each options as option (option)}
+      <option value={option}>{option}</option>
+    {/each}
+  </select>
+</div>
