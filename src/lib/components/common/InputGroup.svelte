ebe5517fa154cbc27a3a5766bf2bf84e4bfbbaa9
Karzan Botani <karzan@botani.nu>
2023-03-27 05:05:27 +0200

    feat(inputgroup): add InputGroup component

---
+++ src/lib/components/common/InputGroup.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+export let value: string | undefined = '';
+export let placeholder = '';
+export let id = '';
+export let label = '';
+export let type = 'text';
+export let disabled = false;
+export let required = false;
+export let errors: string[] | undefined = [];
+
+function handleInput(event: Event): void {
+  if (event instanceof InputEvent && event.target instanceof HTMLInputElement) {
+    value = event.target.value;
+  }
+}
+</script>
+
+<div class="form-control w-full max-w-lg mb-2">
+  <label for={id} class="label font-medium pb-1">
+    <span class="label-text">{label}</span>
+  </label>
+
+  <div class="input-group">
+    <input
+      {disabled}
+      {id}
+      {placeholder}
+      {required}
+      {type}
+      {value}
+      class="input input-bordered w-full max-w-lg"
+      name={id}
+      on:input={handleInput}
+    />
+
+    <button class="btn btn-square" type="button" on:click>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+    </button>
+  </div>
+
+  {#if errors}
+    {#each errors as error}
+      <label for={id} class="label py-0 pt-1">
+        <span class="label-text-alt text-error">{error}</span>
+      </label>
+    {/each}
+  {/if}
+</div>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,6 @@
+export {default as NavBar} from '$lib/components/layout/NavBar.svelte';
 export {default as DatePicker} from '$lib/components/common/DatePicker.svelte';
 export {default as Input} from '$lib/components/common/Input.svelte';
+export {default as InputGroup} from '$lib/components/common/InputGroup.svelte';
 export {default as Select} from '$lib/components/common/Select.svelte';
 export {default as TextArea} from '$lib/components/common/TextArea.svelte';
-export {default as NavBar} from '$lib/components/layout/NavBar.svelte';
