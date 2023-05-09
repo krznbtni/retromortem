3721273314369452c40bc078abdcdc9997287238
Karzan Botani <karzan@botani.nu>
2023-05-09 15:52:50 +0200

    refactor: migrations

---
+++ db/pb_migrations/1683580229_updated_answers.js
@@ -0,0 +1,41 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("oe86c6v8z2gjowx")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "1v4eoqqo",
+    "name": "details",
+    "type": "text",
+    "required": true,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": 1000,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("oe86c6v8z2gjowx")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "1v4eoqqo",
+    "name": "details",
+    "type": "text",
+    "required": true,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": 1000,
+      "pattern": "00"
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
