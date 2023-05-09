12b06e8692ff55ddeae47fc41dba009565776fa1
Karzan Botani <karzan@botani.nu>
2023-05-09 20:34:43 +0200

    refactor: re-implement layout and navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,50 @@
 <script lang="ts">
-import '../app.css';
+import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+import '@skeletonlabs/skeleton/styles/all.css';
+import '../app.postcss';
 
-import NavBar from '$lib/components/layout/NavBar.svelte';
+import {AppBar, AppShell, Drawer, drawerStore} from '@skeletonlabs/skeleton';
+import Navbar from '$lib/components/layout/NavBar.svelte';
+
+function drawerOpen(): void {
+  drawerStore.open({});
+}
 </script>
 
-<div class="min-h-full">
-  <NavBar />
+<Drawer position="right" width="w-[280px] md:w-[400px]">
+  <h2 class="p-4">Navigation</h2>
+  <hr />
+  <Navbar />
+</Drawer>
 
-  <main class="mx-auto max-w-7xl sm:px-6 lg:px-8 px-4">
-    <slot />
-  </main>
-</div>
+<AppShell slotSidebarRight="bg-surface-500/5 w-0 lg:w-64">
+  <svelte:fragment slot="header">
+    <AppBar slotTrail="place-content-end" gap="">
+      <a href="/"><strong class="text-xl uppercase">Retromortem</strong></a>
+
+      <svelte:fragment slot="trail">
+        <div class="flex items-center">
+          <button class="lg:hidden btn btn-sm" on:click={drawerOpen}>
+            <span>
+              <svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+                <rect width="100" height="20" />
+                <rect y="30" width="100" height="20" />
+                <rect y="60" width="100" height="20" />
+              </svg>
+            </span>
+          </button>
+        </div>
+      </svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+
+  <svelte:fragment slot="sidebarRight">
+    <Navbar />
+  </svelte:fragment>
+
+  <slot />
+
+  <svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
 
 <style lang="postcss"></style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container p-10 space-y-4">
+  <h1>Homepage</h1>
+  <hr />
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate nostrum, vero explicabo
+    sunt ipsum asperiores. Molestias nostrum mollitia cumque veniam eum error atque, dolores
+    nesciunt voluptas ea consectetur sed delectus.
+  </p>
 
-<style lang="postcss"></style>
+  <p>
+    Culpa duis dolore magna do aute. Sunt qui nulla non velit irure enim tempor consequat nostrud
+    deserunt consequat officia aliquip. Qui in eiusmod sit laboris nisi labore incididunt laboris
+    pariatur consectetur adipisicing ad.
+  </p>
+
+  <p>
+    Dolore laborum sint cupidatat anim reprehenderit laboris nisi non. Commodo voluptate duis esse
+    id reprehenderit nostrud non. Magna ea quis sint proident. Dolore in anim commodo nisi pariatur
+    culpa anim non cupidatat commodo. Do aute reprehenderit magna ad. Mollit incididunt quis
+    adipisicing eu non occaecat esse sit eiusmod ipsum aliquip est. Dolor ad aliqua ex consectetur
+    amet.
+  </p>
+
+  <p>
+    Consequat id nostrud incididunt cupidatat amet id. Excepteur velit reprehenderit eu commodo
+    irure et laborum. Quis deserunt elit reprehenderit occaecat. Elit est pariatur dolore deserunt
+    dolore deserunt laborum esse dolor cupidatat proident amet. In quis nisi occaecat non. Veniam et
+    et occaecat aute et esse esse elit excepteur pariatur commodo velit ex nisi. Id aliquip non
+    excepteur veniam sint occaecat duis non.
+  </p>
+
+  <p>
+    Pariatur ea ullamco ex fugiat mollit adipisicing ullamco consectetur. Anim in deserunt
+    adipisicing proident. Nostrud duis cillum ipsum nulla. Consequat sint dolor eiusmod pariatur
+    consequat culpa deserunt aliqua duis do quis duis consectetur nisi. Amet aliquip duis veniam ex
+    incididunt velit aliqua reprehenderit exercitation ad eiusmod commodo. Nostrud duis tempor
+    dolore fugiat tempor nulla labore Lorem nisi ad reprehenderit excepteur pariatur cupidatat. Anim
+    sint nisi dolor incididunt ullamco et velit nisi.
+  </p>
+
+  <p>
+    Mollit mollit duis nulla voluptate sit esse. Enim do in officia elit duis officia nostrud aute.
+    Ipsum incididunt esse nisi ullamco reprehenderit labore quis irure eu pariatur velit ullamco.
+    Ullamco ad consequat exercitation in officia ullamco reprehenderit labore mollit labore
+    voluptate culpa ad ex. Exercitation commodo anim exercitation ad aliqua mollit magna culpa
+    aliquip in sint pariatur dolor.
+  </p>
+
+  <p>
+    Consectetur excepteur anim ex ad in culpa ex. Pariatur deserunt dolor sit officia pariatur
+    tempor velit ipsum do eu eiusmod aliquip. Commodo ea elit anim ullamco.
+  </p>
+</div>
